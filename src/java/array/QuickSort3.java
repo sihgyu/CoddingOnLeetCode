b45bda5162f73a126e861b2341da3f47c5e86e7f
yusx <yusx@jiedaibao.com>
2022-03-11 15:21:04 +0800

    LeetCode75

---
+++ src/java/array/QuickSort3.java
@@ -0,0 +1,78 @@
+package array;
+
+/**
+ * @author yusx
+ * @date 2022/3/9 6:47 下午
+ */
+
+public class QuickSort3 {
+
+    // 我们的算法类不允许产生任何实例
+    private QuickSort3() {
+    }
+
+    // 递归使用快速排序,对arr[left...right]的范围进行排序
+    private static void sort(Comparable[] arr, int left, int right) {
+
+        // 对于小规模数组, 使用插入排序会更加快速
+        if (right < left) {
+            //自己实现插入排序
+        }
+
+        /* 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot*/
+        swap(arr, left, (int) (Math.random() * (right - left + 1)) + left);
+
+        Comparable v = arr[left];
+
+        //设置边界值
+        int lt = left;     // arr[left+1...lt] < v
+        int gt = right + 1; // arr[gt...right] > v
+        int i = left + 1;    // arr[lt+1...i) == v
+        while (i < gt) {
+            if (arr[i].compareTo(v) < 0) {
+                swap(arr, i, lt + 1);
+                i++;
+                lt++;
+            } else if (arr[i].compareTo(v) > 0) {
+                swap(arr, i, gt - 1);
+                gt--;
+            } else { // arr[i] == v
+                i++;
+            }
+        }
+
+        swap(arr, left, lt);
+
+        sort(arr, left, lt - 1);
+        sort(arr, gt, right);
+    }
+
+    public static void sort(Comparable[] arr) {
+
+        int n = arr.length;
+        sort(arr, 0, n - 1);
+    }
+
+    //两值交换
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    //输出显示
+    private static void show(Comparable[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + "--");
+        }
+    }
+
+    // 测试 QuickSort3
+    public static void main(String[] args) {
+
+        Comparable[] arr = new Comparable[]{23, 45, 1, 21, 67, 78, 89, 55, 12, 34, 22, 11, 5, 6, 9};
+        sort(arr);
+        show(arr);
+        return;
+    }
+}
