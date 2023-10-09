acde9ffeedae4e906d39d4619bec51d2544209af
sihgyu <sihgyu@foxmail.com>
2023-10-09 23:07:44 +0800

    LeetCode141

---
+++ src/java/linkedList/ListNode.java
@@ -0,0 +1,17 @@
+package linkedList;
+
+/**
+ * @author :yusx
+ * @date : 2023/10/9
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
